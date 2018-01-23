e031366664997b35d928f8c53e95cf393d72c827
Kurtis Carpenter <kurtiscarpenter@kurtiss-mbp.attlocal.net>
2018-01-22 23:20:25 -0500

    get it bro

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -6,83 +6,94 @@ var countdown = 10;
 var index = 0;
 var canPress = true;
 clockRunning = false;
+var timerHandle;
 
 //questions in the game
 //make this an object instead of an array. keep questions as an array.
 var quiz = {
-    questions: ["George Washington lied to his father when he cut down the cherry tree", 
+    questions: ["George Washington lied to his father when he cut down the cherry tree",
         "Is purple the color of Kings?.",
         "Kurtis Carpenter is a man, myth and ledgend",
         "You are grading this project.",
-        "Forest Gump was the greatest movie ever made"],
+        "Forrest Gump was the greatest movie ever made"],
     answers: [false, true, true, true, true]
-  };
+};
 
- //prints questions to page and console.logs it as well
-$(document).ready(function(){
+//prints questions to page and console.logs it as well
+$(document).ready(function () {
 
-function printQuestion(){
-   
-    if(index <= quiz.questions.length){
-        $("#questions").text(quiz.questions[index]);
-        clockRunning = true;
-        canPress = true;
+    function printQuestion() {
+        clearInterval(timerHandle);
+        console.log(index);
+        if (index < quiz.questions.length) {
+            $("#questions").text(quiz.questions[index]);
+            clockRunning = true;
+            canPress = true;
+            startTimer();
+        }
+        else {
+            console.log(quiz.questions[index]);
+            $("#update").text("Game Over");
+            clockRunning = false;
+            canPress = false;
+        }
+
     }
-    else {
-        console.log(quiz.questions[index]);
-        alert("game over");
-        clockRunning = false;
-        canPress=false;
-    }
 
-}
+    document.onkeyup = function (event) {
+        if (canPress === true) {
 
-document.onkeyup = function(event){
-    if(canPress === true){
+            if (event.key === "t" || event.key === "f") {
+                // if(event.key === "t"){
+                //     var userChoice = true;
+                // } else{
+                //     var userChoice = false;
+                // }
+                var userChoice = (event.key === "t") ? true : false;
 
-        if(event.key === "t" || event.key === "f"){
-            // if(event.key === "t"){
-            //     var userChoice = true;
-            // } else{
-            //     var userChoice = false;
-            // }
-            var userChoice = (event.key === "t") ? true : false;
+                if (userChoice === quiz.answers[index]) {
+                    $("#update").text("correct")
+                    score++;
+                    console.log(score);
+                    $("#score").text("Current Score: " + score + " points");
+                } else {
+                    $("#update").text("you're wrong");
+                    $("#score").text("Current Score: " + score + " points");
+                }
 
-            if(userChoice === quiz.answers[index]){
-                alert("you're Correct!");
-                score++;
-                console.log(score);
-                $("#score").text("Current Score: " + score + " points");
-            } else{
-                alert("you're wrong");
-                $("#score").text("Current Score: " + score + " points");
+                index++;
+                printQuestion();
+
+
             }
-        
-            index++;
-            printQuestion();
-           
-            
         }
     }
-}
 
 
-//function that starts the timer 
+    //function that starts the timer 
+    //set timer equal to 10 sec
+    //make timer clock decrease by 1 second per 
+    //if timer runs out before questions is answered, then next question
+    //if timer does not run out before questions is answered, then question answer, 
+    //add points in necessary, then next question, start timer over
+    function startTimer() {
+        countdown = 10;
+        $("#timer").text(countdown + " seconds left to answer");        
+        timerHandle = setInterval(function () {
+            countdown--;
+            if (countdown > 0) {
+                $("#timer").text(countdown + " seconds left to answer");
+            }
+            else if (countdown === 0) {
+                clearInterval(timerHandle);
+                index++;
+                printQuestion();
 
-setInterval (function(){
-    countdown--;
-    if(countdown >= 0){
-        $("#timer").text(countdown + " seconds left to answer");
-       
+            }
+        }, 1000);
     }
-    else if(countdown === 0){
-        clearInterval(counter);
-        index++;
-        printQuestion();
-    }
-    }, 1000);
 
-printQuestion();
+    printQuestion();
 
 
 });
@@ -94,5 +105,5 @@ printQuestion();
 
 
 
-    
-//function that renders the questions
+
+
